0e32d1fbd1b790e44e11d90eb8a504d494654420
Nicolas Blaisdell <nblaisdell2@gmail.com>
2023-07-15 20:43:33 -0400

    enhanced github actions script to check for needed 'secret' values in repo

---
+++ check_github_secrets.sh
@@ -0,0 +1,33 @@
+shouldFail=0
+
+if [[ $1 == "" ]]
+then
+  echo "{AWS_REGION} needs to be added to the Secrets in the GitHub repo"
+  shouldFail=1  
+fi
+
+if [[ $2 == "" ]]
+then
+  echo "{AWS_ACCOUNT_ID} needs to be added to the Secrets in the GitHub repo"
+  shouldFail=1  
+fi
+
+if [[ $3 == "" ]]
+then
+  echo "{AWS_LAMBDA_EXEC_ARN} needs to be added to the Secrets in the GitHub repo"
+  shouldFail=1  
+fi
+
+if [[ $4 == "" ]]
+then
+  echo "{AWS_GHACTIONS_ROLENAME} needs to be added to the Secrets in the GitHub repo"
+  shouldFail=1  
+fi
+
+if [[ $shouldFail -eq 1 ]]
+then
+  echo "Stopping build..."
+  exit 1
+else
+  echo "All needed values found. Continuing build..."
+fi
