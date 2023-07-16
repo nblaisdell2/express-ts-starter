afb5bf67759903d6473aaa89b02eb8323176f607
Nicolas Blaisdell <nblaisdell2@gmail.com>
2023-07-16 01:30:24 -0400

    testing updated code & test

--- src/app.test.ts
+++ src/app.test.ts
@@ -12,7 +12,7 @@ describe("The Main API", () => {
         expect(response.body).toEqual(
           expect.objectContaining({
             customMessage: expect.objectContaining({
-              msg: "Hello",
+              msg: "Hello v2",
             }),
           })
         );
--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -7,7 +7,7 @@ export const helloWorld = function (
   next: NextFunction
 ) {
   let customMessage: Message = {
-    msg: "Hello",
+    msg: "Hello v2",
   };
   res.status(200).json({ customMessage });
 };
